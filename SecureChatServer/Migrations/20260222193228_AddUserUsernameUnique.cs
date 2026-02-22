28604b6ddd9673cb939a5b5b293045f340b2d12b
AlonSheidin <alon.sheidin@gmail.com>
2026-02-22 23:30:43 +0200

    Add unique constraint to Username in users table

---
+++ SecureChatServer/Migrations/20260222193228_AddUserUsernameUnique.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SecureChatServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserUsernameUnique : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("Npgsql:PostgresExtension:citext", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "users",
+                type: "citext",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "text");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Username",
+                table: "users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_users_Username",
+                table: "users");
+
+            migrationBuilder.AlterDatabase()
+                .OldAnnotation("Npgsql:PostgresExtension:citext", ",,");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Username",
+                table: "users",
+                type: "text",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "citext");
+        }
+    }
+}
